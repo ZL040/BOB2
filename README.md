96980482eb98331b516db2cf26d4275d0d49083b
ZL040 <458042473@qq.com>
2021-08-11 21:18:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# study
+# BOB
+# BOB2
+# BOB2
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("执行App类addNumber()方法");
+        int n1 = 10;
+        int n2 = 20;
+        int res = 0;
+        App app = new App();
+        res = app.add(n1,n2);
+        Assert.assertEquals(30,res);
+        assertTrue( true );
+    }
+
+}
